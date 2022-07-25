f75fbc2cfcd06af4d5ce73cf85b1e29f4fa3d1b6
Nathan Brown <43689712+OxxoCodes@users.noreply.github.com>
2022-07-24 23:14:38 -0400

    Added my resume

--- src/WhoAmI.js
+++ src/WhoAmI.js
@@ -9,7 +9,7 @@ const WhoAmI = () => {
       <p>I've been developing software for nearly a decade, and have dabbled in topics ranging from Web Development to Pentesting to Quantitative Historical Stock Analysis.</p>
       <p>If there is code to be written, I will write it.</p>
       <p>I'm also an Eagle Scout, an <Link url="https://www.instagram.com/oxxodraws/" text="artist"/>, a runner, and a retro games and hardware enthusiast.</p>
-      <p>You can download my resume <Link url="https://www.youtube.com/watch?v=dQw4w9WgXcQ" text="here"/>.</p>
+      <p>You can download my resume <Link url="Nathan Brown Resume.pdf" text="here"/>.</p>
     </div>
   )
 }
