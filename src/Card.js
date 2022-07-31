3a5ed8fa029d32238e44539c1db559d89b975a01
Nathan Brown <43689712+OxxoCodes@users.noreply.github.com>
2022-07-30 23:30:03 -0400

    Added rough opening/closing animation to Cards

--- src/Card.js
+++ src/Card.js
@@ -2,25 +2,32 @@ import React from "react"
 import {motion} from "framer-motion"
 import CardWrapper from "./CardWrapper";
 
+export const openSpring = { type: "spring", stiffness: 200, damping: 30 };
+export const closeSpring = { type: "spring", stiffness: 300, damping: 35 };
+
+
 const Card = ({header, body, image}) => {
-  const [flipped, setFlipped] = React.useState(false)
+  const [clicked, setClicked] = React.useState(false)
 
   let variants = {
-    notFlipped: {rotateY:0, transition: {duration:0.5}},
-    flipped: {rotateY:180, transition: {duration:0.5}}
+    closed: {width:'300px', height:'500px', position:'relative', display:'block'},
+    open: {width:'calc(100vw - 80px)', height:'calc(100vh - 80px)', top:'0', left:'0',
+           margin:'40px', position:'fixed', zIndex:'2', overflow:'hidden'}
   }
 
   return (
     <CardWrapper filename={image}>
       <motion.div className="card" whileHover={{scale:1.01}}
-                  animate={flipped ? 'flipped' : 'notFlipped'}
-                  variants={variants}>
-                  {/*onClick={() => setFlipped(!flipped)}>*/}
+                  animate={clicked ? 'open' : 'closed'}
+                  variants={variants}
+                  layoutTransition={clicked ? openSpring : closeSpring}
+                  onClick={() => setClicked(true)}>
         <div className="card-front-text">
           <h1 className="card-header">{header}</h1>
           <p className="card-body">{body}</p>
         </div>
       </motion.div>
+      {clicked && <div style={{position:'fixed', zIndex:'1', top:'0', left:'0', width:'100vw', height:'100vh'}} onClick={()=>setClicked(false)}/>}
     </CardWrapper>
   )
 }
