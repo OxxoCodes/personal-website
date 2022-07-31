dd672ed1c4bc2a5c6dacf6a7e2e3a2520fc60ce2
Nathan Brown <43689712+OxxoCodes@users.noreply.github.com>
2022-07-30 22:06:34 -0400

    Drastically reduced overall site size by optimizing images

--- src/WhatHaveIBuilt.js
+++ src/WhatHaveIBuilt.js
@@ -9,16 +9,16 @@ const WhatHaveIBuilt = () => {
       </div>
       <div style={{display: "flex", justifyContent:"center", flexDirection:"column"}}>
         <div style={{display:"flex", justifyContent:'space-around', flexWrap: 'wrap'}}>
-          <Card header={"Hydrogen"} body={"A portfolio optimization and backtesting application"} image={"Hydrogen.png"}/>
-          <Card header={"DriveMan"} body={"An application for managing files stored on Google Drive"} image={"DriveMan.png"}/>
-          <Card header={"Kano"} body={"A CHIP-8 Interpreter"} image={"Kano.png"}/>
-          <Card header={"This Website"} body={"Take a look around!"} image={"Me.png"}/>
+          <Card header={"Hydrogen"} body={"A portfolio optimization and backtesting application"} image={"Hydrogen.jpg"}/>
+          <Card header={"DriveMan"} body={"An application for managing files stored on Google Drive"} image={"DriveMan.jpg"}/>
+          <Card header={"Kano"} body={"A CHIP-8 Interpreter"} image={"Kano.jpg"}/>
+          <Card header={"This Website"} body={"Take a look around!"} image={"Me.jpg"}/>
         </div>
         <div style={{display:"flex", justifyContent:'space-around', flexWrap: 'wrap'}}>
-          <Card header={"Internal Enterprise Application"} body={"An internal web application at Ally Financial, built using React"} image={"Ally.png"}/>
-          <Card header={"Arcade Cabinet"} body={"A completely homemade from-scratch full-size arcade cabinet, built using a Raspberry Pi 4"} image={"Arcade.png"}/>
-          <Card header={"VRFE"} body={"A multi-platform, mobile VR Grand Canyon exploration simulation"} image={"VRFE.png"}/>
-          <Card header={"MarketBot"} body={"A Discord-based sales application"} image={"MarketBot.png"}/>
+          <Card header={"Internal Enterprise Application"} body={"An internal web application at Ally Financial, built using React"} image={"Ally.jpg"}/>
+          <Card header={"Arcade Cabinet"} body={"A completely homemade from-scratch full-size arcade cabinet, built using a Raspberry Pi 4"} image={"Arcade.jpg"}/>
+          <Card header={"VRFE"} body={"A multi-platform, mobile VR Grand Canyon exploration simulation"} image={"VRFE.jpg"}/>
+          <Card header={"MarketBot"} body={"A Discord-based sales application"} image={"MarketBot.jpg"}/>
         </div>
       </div>
     </div>
