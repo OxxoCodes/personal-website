f210ee820f110da43df5568fc8bdcb39babcd62e
Nathan Brown <43689712+OxxoCodes@users.noreply.github.com>
2022-08-04 23:25:35 -0400

    Removed duplicate text in VRFE card

--- src/Hello/Hello.js
+++ src/Hello/Hello.js
@@ -32,7 +32,7 @@ const Hello = () => {
         <p>I'm a driven college student with a passion for nearly anything code.</p>
         <p>I've been developing software for nearly a decade, and have dabbled in topics ranging from Web Development to Pentesting to Quantitative Historical Stock Analysis.</p>
         <p>If there is code to be written, I will write it.</p>
-        <p>I'm also an Eagle Scout, an <Link url="https://www.instagram.com/oxxodraws/" text="artist"/>, a runner, and a retro games and hardware enthusiast.</p>
+        <p>I'm also an Eagle Scout, an artist, a runner, and a retro games and hardware enthusiast.</p>
         <p>You can download my resume <Link url="Nathan Brown Resume.pdf" text="here"/>.</p>
       </div>
     </div>
