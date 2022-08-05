82d56574066f741072a35004f91c23f9d92d57b0
Nathan Brown <43689712+OxxoCodes@users.noreply.github.com>
2022-08-04 23:41:20 -0400

    Filled out ThisWebsite card

--- src/WhatHaveIBuilt/ThisWebsite/ThisWebsite.js
+++ src/WhatHaveIBuilt/ThisWebsite/ThisWebsite.js
@@ -7,7 +7,15 @@ const ThisWebsite = ({filename}) => {
     <ThisWebsiteWrapper filename={filename}>
       <div className="text">
         <h1>This Website</h1>
-        <p>More info coming soon!</p>
+        <p>Take a look around!</p>
+        <p>This website began development as I approached the end of my software development internship at Ally. Prior to
+           that internship, I had never touched web development, but had always liked the idea of putting together a
+           portfolio site where I could not only more interestingly present my past work, but also have a space for
+           potential experiments, web applications, and blog posts.</p>
+        <p>Once I gained the skills I found necessary to get this project up-and-running, I immediately got to work!
+           This site is very much still under construction as I alter and refine the overall design, implement new features
+           and animations, and create new content!</p>
+        <p>This site is built using JavaScript, React, Framer Motion, and Material UI.</p>
       </div>
       <div className="image" />
     </ThisWebsiteWrapper>
