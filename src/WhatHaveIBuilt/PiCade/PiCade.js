55da17eae395e569473c5a816b8fff9cd6772781
Nathan Brown <43689712+OxxoCodes@users.noreply.github.com>
2022-08-04 23:26:01 -0400

    Filled out PiCade card

--- src/WhatHaveIBuilt/PiCade/PiCade.js
+++ src/WhatHaveIBuilt/PiCade/PiCade.js
@@ -7,7 +7,18 @@ const PiCade = ({filename}) => {
     <PiCadeWrapper filename={filename}>
       <div className="text">
         <h1>Pi-Cade</h1>
-        <p>More info coming soon!</p>
+        <p>Short for "Raspberry Pi Arcade", Pi-Cade is a homemade, completely from-scratch full-size and tabletop arcade
+           cabinet.</p>
+        <p>The star of the show here is a Raspberry Pi 4 4GB, which runs Retroarch for emulation and EmulationStation for
+           the frontend through <Link url={"https://retropie.org.uk/"} text={"RetroPie"}/>.</p>
+        <p>The cabinet features two-player support, with one 8-way joystick and six buttons per player, a working coin
+           slot, powerful stereo speakers, customizable RGB LEDs, USB, and storage space for external peripherals - all
+           wrapped up in a completely custom design.</p>
+        <p>The top half of the cabinet is also completely detachable from the bottom half. This means that, depending on
+           your desired use case, you can go from having a full-size arcade cabinet to a tableetop without losing any of
+           the important features! Minus the coin slot, of course.</p>
+        <p>The frame was 3D modeled prior to being cut out of MFD wood. All vinyl was custom-printed using a custom
+           compilation of arcade-related artwork.</p>
       </div>
       <div className="image" />
     </PiCadeWrapper>
