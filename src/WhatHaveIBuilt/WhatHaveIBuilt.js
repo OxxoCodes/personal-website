bdb2f34b492032e8f1b8c7d5d152d690dcf6e901
Nathan Brown <43689712+OxxoCodes@users.noreply.github.com>
2022-08-08 13:31:06 -0400

    fix: Flex wrapping on cards with certain viewport widths

--- src/WhatHaveIBuilt/WhatHaveIBuilt.js
+++ src/WhatHaveIBuilt/WhatHaveIBuilt.js
@@ -11,58 +11,54 @@ import MarketBot from "./MarketBot/MarketBot";
 
 const WhatHaveIBuilt = () => {
   const data = [
-    [
-      {
-        header: "Hydrogen",
-        previewBody: "A portfolio optimization and backtesting application",
-        body: <Hydrogen filename={"Hydrogen.jpg"}/>,
-        image: "Hydrogen.jpg"
-      },
-      {
-        header: "DriveMan",
-        previewBody: "An application for managing files stored on Google Drive",
-        body: <DriveMan filename={"DriveMan.jpg"} />,
-        image: "DriveMan.jpg"
-      },
-      {
-        header: "Kano",
-        previewBody: "A CHIP-8 Interpreter",
-        body: <Kano filename={"Kano.jpg"} />,
-        image: "Kano.jpg"
-      },
-      {
-        header: "This Website",
-        previewBody: "Take a look around!",
-        body: <ThisWebsite filename={"Me.jpg"} />,
-        image: "Me.jpg"
-      }
-    ],
-    [
-      {
-        header: "Internal Enterprise Application",
-        previewBody: "An internal web application at Ally Financial, built using React",
-        body: <IEA filename={"Ally.jpg"} />,
-        image: "Ally.jpg"
-      },
-      {
-        header: "Pi-Cade",
-        previewBody: "A completely homemade from-scratch full-size arcade cabinet, built using a Raspberry Pi 4",
-        body: <PiCade filename={"Arcade.jpg"} />,
-        image: "Arcade.jpg"
-      },
-      {
-        header: "VRFE",
-        previewBody: "A multi-platform, mobile VR Grand Canyon exploration simulation",
-        body: <Vrfe filename={"VRFE.jpg"} />,
-        image: "VRFE.jpg"
-      },
-      {
-        header: "MarketBot",
-        previewBody: "A Discord-based sales application",
-        body: <MarketBot filename={"MarketBot.jpg"} />,
-        image: "MarketBot.jpg"
-      }
-    ]
+    {
+      header: "Hydrogen",
+      previewBody: "A portfolio optimization and backtesting application",
+      body: <Hydrogen filename={"Hydrogen.jpg"}/>,
+      image: "Hydrogen.jpg"
+    },
+    {
+      header: "DriveMan",
+      previewBody: "An application for managing files stored on Google Drive",
+      body: <DriveMan filename={"DriveMan.jpg"} />,
+      image: "DriveMan.jpg"
+    },
+    {
+      header: "Kano",
+      previewBody: "A CHIP-8 Interpreter",
+      body: <Kano filename={"Kano.jpg"} />,
+      image: "Kano.jpg"
+    },
+    {
+      header: "This Website",
+      previewBody: "Take a look around!",
+      body: <ThisWebsite filename={"Me.jpg"} />,
+      image: "Me.jpg"
+    },
+    {
+      header: "Internal Enterprise Application",
+      previewBody: "An internal web application at Ally Financial, built using React",
+      body: <IEA filename={"Ally.jpg"} />,
+      image: "Ally.jpg"
+    },
+    {
+      header: "Pi-Cade",
+      previewBody: "A completely homemade from-scratch full-size arcade cabinet, built using a Raspberry Pi 4",
+      body: <PiCade filename={"Arcade.jpg"} />,
+      image: "Arcade.jpg"
+    },
+    {
+      header: "VRFE",
+      previewBody: "A multi-platform, mobile VR Grand Canyon exploration simulation",
+      body: <Vrfe filename={"VRFE.jpg"} />,
+      image: "VRFE.jpg"
+    },
+    {
+      header: "MarketBot",
+      previewBody: "A Discord-based sales application",
+      body: <MarketBot filename={"MarketBot.jpg"} />,
+      image: "MarketBot.jpg"
+    }
   ]
 
   return (
@@ -70,16 +66,10 @@ const WhatHaveIBuilt = () => {
       <div style={{display:'flex', flexDirection:'column', textAlign:'center'}}>
         <h1>💻 What Have I Built?</h1>
       </div>
-      <div style={{display: "flex", justifyContent:"space-around", flexDirection:"column"}}>
+      <div style={{display: "flex", justifyContent:"space-around", flexWrap:"wrap"}}>
         {
-          data.map((row) =>
-            <div style={{display:"flex", justifyContent:'space-around', flexWrap: 'wrap'}}>
-              {
-                row.map((c, index) =>
-                  <Card header={c.header} previewBody={c.previewBody} body={c.body} image={c.image} cid={index} />
-                )
-              }
-            </div>
+          data.map((c, index) =>
+            <Card header={c.header} previewBody={c.previewBody} body={c.body} image={c.image} cid={index} />
           )
         }
       </div>
