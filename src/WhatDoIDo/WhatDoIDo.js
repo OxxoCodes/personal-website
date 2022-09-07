ca2708dfc3e1039d34a78f7f586fbc93a71abe23
Nathan Brown <43689712+OxxoCodes@users.noreply.github.com>
2022-09-07 18:34:09 -0400

    Updated What Do I Do text to mirror current work

--- src/WhatDoIDo/WhatDoIDo.js
+++ src/WhatDoIDo/WhatDoIDo.js
@@ -7,7 +7,9 @@ const WhatDoIDo = () => {
       <h1>What Do I Do? 📝</h1>
       <Typing text={["React", "JavaScript", "Java", "Python", "C/C++", "C#", "Teach", "Research", "Virtual Reality", "Binary Exploitation", "Reverse Engineering", "Unity", "TensorFlow"]}/>
       <p>I learn. I work fast. I write quality, comprehensible code.</p>
-      <p>Currently doing web development at Ally Financial.</p>
+      <p>Currently a security analyst at Clemson University's Cybersecurity Operations Center.</p>
+      <p>Doing NLP and NER model optimization for Giant Oak's GOSTCrawl through Clemson's Fall 2022 Capstone Program.</p>
+      <p>Previous a frontend developer at Ally Financial, a VR research assistant, and a teaching assistant.</p>
     </div>
   )
 }
